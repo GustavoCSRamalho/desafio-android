fead3f0e0c995abb50b5658d9d838e831219e023
Gustavo Ramalho <gstvramalho@gmail.com>
2021-01-13 14:55:21 -0300

    Dependencies added to build.gradle

---
+++ app/src/main/java/com/gustavo/desafio_android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gustavo.desafio_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gustavo.desafio_android.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
